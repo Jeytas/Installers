adbc24658eb85504ee94c4df54aa02499f96bca0
Marvin Johanning <manimusmuellus@gmail.com>
2017-11-07 19:28:58 +0100

    Added another safety feature, cannot install when installed previously and not removed aliases from .bashrc

--- installer.sh
+++ installer.sh
@@ -7,13 +7,20 @@ echo "Cloning from repository starting"
 git clone https://github.com/Jeytas/RuXt.git ~/.ruxt
 if [ $? == 0 ]; then echo "Cloning successful"; else echo "Cloning failed" && exit; fi
 
-alias xt="ruby ~/.ruxt/extract.rb"
-alias updatext="bash ~/.ruxt/update.sh"
-if [ $? == 0 ]; then echo "Setting aliases (xt and updatext) in current bash session was successful"; else echo "Setting aliases in current session was unsuccessful" && exit; fi
+if grep -q ".ruxt" ~/.bashrc
+    then
+        echo "It seems like you've already installed this program before and removed it, but forgot to remove the aliases from your .bashrc. To prevent having several of the same aliases in your .bashrc, please remove the aliases manually from your .bashrc and run this script again" && exit
 
-echo 'alias updatext="bash ~/.ruxt/update.sh"' >> ~/.bashrc
-echo 'alias xt="ruby ~/.ruxt/extract.rb"' >> ~/.bashrc
-if [ $? == 0 ]; then echo "Setting alias in ~/.bashrc has was successful"; else echo "Setting aliases in ~/.bashrc was unsuccessful" && exit; fi
+    else
+        alias xt="ruby ~/.ruxt/extract.rb"
+        alias updatext="bash ~/.ruxt/update.sh"
+        if [ $? == 0 ]; then echo "Setting aliases (xt and updatext) in current bash session was successful"; else echo "Setting aliases in current session was unsuccessful" && exit; fi
 
-echo "If you see this message, everything should've been installed correctly. To ensure that it has, please run 'xt -h' and if you get some help regarding the program everything has been installed correctly. If this doesn't work, please try restarting your current Terminal. If that still doesn't solve the issue, please create anissue on this project's Github"
-echo "To update RuXt, please run updatext"
+        echo 'alias updatext="bash ~/.ruxt/update.sh"' >> ~/.bashrc
+        echo 'alias xt="ruby ~/.ruxt/extract.rb"' >> ~/.bashrc
+        if [ $? == 0 ]; then echo "Setting alias in ~/.bashrc has was successful"; else echo "Setting aliases in ~/.bashrc was unsuccessful" && exit; fi
+
+        echo "If you see this message, everything should've been installed correctly. To ensure that it has, please run 'xt -h' and if you get some help regarding the program everything has been installed correctly. If this doesn't work, please try restarting your current Terminal. If that still doesn't solve the issue, please create anissue on this project's Github"
+        echo "To update RuXt, please run updatext"
+fi
+
